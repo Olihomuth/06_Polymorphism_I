7f5daea773e7b7c3f221a19d0ec23a0f3f0eba1a
Oliver <olihomuth1@gmx.de>
2025-11-26 13:16:59 +0100

    06 / Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,24 +3,52 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
         Queen queen = new Queen();
         Worker worker = new Worker();
         Drone drone = new Drone();
-
-        output(queen.doYourJob());
-        output(queen.fly());
-        output("-------------------");
-
-        output(worker.doYourJob());
-        output(worker.fly());
-        output("-------------------");
-
-        output(drone.doYourJob());
-        output(drone.fly());
-        output("-------------------");
-
         
+        // Erste Iteration
+        // output(queen.doYourJob());
+        // output(queen.fly());
+        // output("-------------------");
+        // output(worker.doYourJob());
+        // output(worker.fly());
+        // output("-------------------");
+        // output(drone.doYourJob());
+        // output(drone.fly());
+        // output("-------------------");
+
+
+        pollObject(queen);
+        pollObject(worker);
+        pollObject(drone);
+        
+    }
+
+    //  Zweite Iteration überladene Methoden
+    // private static void pollObject(Queen obj){
+    //     output(obj.doYourJob());
+    //     output(obj.fly());
+    //     output("-------------------");
+    // }
+    // private static void pollObject(Worker obj){
+    //     output(obj.doYourJob());
+    //     output(obj.fly());
+    //     output("-------------------");
+    // }
+    // private static void pollObject(Drone obj){
+    //     output(obj.doYourJob());
+    //     output(obj.fly());
+    //     output("-------------------");
+    // }
+
+
+    // Dritte Iteration Polymorphie
+    private static void pollObject(HoneyBee obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("-------------------");
     }
 
    
--- com/cc/java/Queen.java
+++ com/cc/java/Queen.java
@@ -2,4 +2,8 @@ package com.cc.java;
 
 public class Queen extends HoneyBee {
 
+    public String doYourJob(){
+        return "I'm a Queen, any questions?";
+    }
+
 }
